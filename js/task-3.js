1c7e16fbae6828a0aeb45a4a8f453d7074197ebb
Iryna Serhiienko <ivus0888@gmail.com>
2024-02-10 22:05:27 +0200

    init-hw-03

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,27 @@
-// function checkForSpam(message) {
-//     let result;
-//     result = message.toLowerCase(message).includes(`spam`) ||
-//         message.toLowerCase(message).includes(`sale`);
-//     return result;
-// }
-function checkForSpam(message) {
-  const normalizedMessage = message.toLowerCase();
-  return (
-    normalizedMessage.includes('spam') || normalizedMessage.includes('sale')
-  );
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (let i = 0; i <= numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray;
 }
 
-checkForSpam('Latest technology news');
-checkForSpam('JavaScript weekly newsletter');
-checkForSpam('Get best sale offers now!');
-checkForSpam('Amazing SalE, only tonight!');
-checkForSpam('Trust me, this is not a spam message');
-checkForSpam('Get rid of sPaM emails. Our book in on sale!');
-checkForSpam('[SPAM] How to earn fast money?');
+// 2 - вариант
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// function filterArray(numbers, value) {
+//   const newArray = [];
+//   for (const item of numbers) {
+//     if (item > value) {
+//       newArray.push(item);
+//     }
+//   }
+//   return newArray;
+// }
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
