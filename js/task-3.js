32253100d0f960d37170f5491615f8076570baab
Iryna Serhiienko <109947833+irinaivus@users.noreply.github.com>
2024-02-10 20:29:06 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+// function checkForSpam(message) {
+//     let result;
+//     result = message.toLowerCase(message).includes(`spam`) ||
+//         message.toLowerCase(message).includes(`sale`);
+//     return result;
+// }
+function checkForSpam(message) {
+  const normalizedMessage = message.toLowerCase();
+  return (
+    normalizedMessage.includes('spam') || normalizedMessage.includes('sale')
+  );
+}
+
+checkForSpam('Latest technology news');
+checkForSpam('JavaScript weekly newsletter');
+checkForSpam('Get best sale offers now!');
+checkForSpam('Amazing SalE, only tonight!');
+checkForSpam('Trust me, this is not a spam message');
+checkForSpam('Get rid of sPaM emails. Our book in on sale!');
+checkForSpam('[SPAM] How to earn fast money?');
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
